d6d3de982f6522c472f71b4e5391783906841f18
Gourav Nayyar <gourav.n@gmail.com>
2017-01-29 15:16:17 +0530

    - Updated the code to swift 3.0 - Added the sound with count down.

--- GameLaunchTimer/TimerView.swift
+++ GameLaunchTimer/TimerView.swift
@@ -11,40 +11,45 @@ let TIMERVIEW_RADIUS:CGFloat = 50
 let TIMER_LABEL_INITIAL_VAL:Int =  3
 let BORDER_WIDTH:CGFloat = 2
 var timerVal:Int = TIMER_LABEL_INITIAL_VAL;
-var timer:NSTimer!
+var timer:Timer!
 
 
 import UIKit
 import QuartzCore
+import AVFoundation
 
 class TimerView :UIView {
   
+    var audioPlayer : AVAudioPlayer = AVAudioPlayer()
+ 
+    
+    
   struct Stored {
     static var timerLbl:UILabel!
   }
 
   
-  class func loadingCountDownTimerViewInView (_superView:UIView)-> TimerView
+  class func loadingCountDownTimerViewInView (_ _superView:UIView)-> TimerView
   {
-    var timerView:TimerView = TimerView(frame:_superView.frame)
-    timerView.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(VIEW_ALPHA)
+    let timerView:TimerView = TimerView(frame:_superView.frame)
+    timerView.backgroundColor = UIColor.black.withAlphaComponent(VIEW_ALPHA)
     _superView.addSubview(timerView)
     
     /* add a custom Circle view */
     
-    let refFrame:CGRect = CGRectMake(_superView.center.x-TIMERVIEW_RADIUS, _superView.center.y-TIMERVIEW_RADIUS, 2*TIMERVIEW_RADIUS, 2*TIMERVIEW_RADIUS)
-    var circleView:UIView = UIView(frame:refFrame)
+    let refFrame:CGRect = CGRect(x: _superView.center.x-TIMERVIEW_RADIUS, y: _superView.center.y-TIMERVIEW_RADIUS, width: 2*TIMERVIEW_RADIUS, height: 2*TIMERVIEW_RADIUS)
+    let circleView:UIView = UIView(frame:refFrame)
     circleView.layer.cornerRadius = TIMERVIEW_RADIUS
-    circleView.layer.borderColor = UIColor.whiteColor().CGColor
+    circleView.layer.borderColor = UIColor.white.cgColor
     circleView.layer.borderWidth = BORDER_WIDTH
     
     /* add a custom Label */
     
     Stored.timerLbl = UILabel(frame:circleView.bounds)
     Stored.timerLbl.text = "\(TIMER_LABEL_INITIAL_VAL)"
-    Stored.timerLbl.textColor = UIColor.whiteColor()
+    Stored.timerLbl.textColor = UIColor.white
     Stored.timerLbl.font = UIFont(name: "MarkerFelt-Thin", size: 40)
-    Stored.timerLbl.textAlignment = NSTextAlignment.Center
+    Stored.timerLbl.textAlignment = NSTextAlignment.center
     
     circleView.addSubview(Stored.timerLbl)
     timerView.addSubview(circleView)
@@ -52,15 +57,32 @@ class TimerView :UIView {
     return timerView
   }
   
+    func initiateSound() {
+        let pathToFile = Bundle.main.path(forResource: "soundFile", ofType: "mp3")
+        
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOf: URL(fileURLWithPath: pathToFile!) as URL)
+        } catch {
+            print("Failed to assign path to audio player.")
+        }
+    }
+    
+    func play() {
+        audioPlayer.play()
+    }
+    
   func startTimer()
   {
-    timer = NSTimer.scheduledTimerWithTimeInterval(1.0
-      , target: self, selector: Selector("updateTimer:"), userInfo: nil, repeats: true)
+    self.initiateSound()
+    audioPlayer.play()
+    timer = Timer.scheduledTimer(timeInterval: 1.0
+      , target: self, selector: #selector(TimerView.updateTimer(_:)), userInfo: nil, repeats: true)
   }
   
-  func updateTimer(dt:NSTimer)
+  func updateTimer(_ dt:Timer)
   {
-    timerVal--
+    audioPlayer.play()
+    timerVal -= 1
     if timerVal==0{
       Stored.timerLbl.text = "Go"
     }else if timerVal<0{
@@ -73,8 +95,9 @@ class TimerView :UIView {
   
   func removeCountDownTimerView()
   {
-    var mySuperView:UIView = self.superview
-    mySuperView.userInteractionEnabled = true
+    timerVal = TIMER_LABEL_INITIAL_VAL
+    let mySuperView:UIView = self.superview!
+    mySuperView.isUserInteractionEnabled = true
     super.removeFromSuperview()
   }
 }
--- GameLaunchTimer/ViewController.swift
+++ GameLaunchTimer/ViewController.swift
@@ -13,17 +13,20 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-    
-    /* load a count down view */
-    var gamelaunchTimerView:TimerView = TimerView.loadingCountDownTimerViewInView(self.view)
-    gamelaunchTimerView.startTimer()
   }
-
+    
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
 
+    
+    @IBAction func startTimerClicked(_ sender: Any) {
+        /* load a count down view */
+        let gamelaunchTimerView:TimerView = TimerView.loadingCountDownTimerViewInView(self.view)
+        gamelaunchTimerView.startTimer()    
+    }
+    
 
 }
 
